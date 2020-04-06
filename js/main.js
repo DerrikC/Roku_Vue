f7f41248a99ef4aa81e4544a33689ebfa57f20bf
Derrik Campbell <derrikcampbell@hotmail.com>
2020-04-06 17:28:10 -0400

    Linking PHP to designed user login

--- js/main.js
+++ js/main.js
@@ -1,59 +1,51 @@
-// todo => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
 
-var vm = new Vue({
-  el: "#app",
+import AllUsersComponent from './components/AllUsersComponent.js';
+import LoginComponent from './components/LoginComponent.js';
+import UserHomeComponent from './components/UserHomeComponent.js';
 
-  data: {
+(() => {
+  let router = new VueRouter({
+    // set routes
+    routes: [
+      { path: '/', redirect: { name: "login" } },
+      { path: '/login', name: "login", component: LoginComponent },
+      { path: '/users', name: 'users', component: AllUsersComponent },
+      { path: '/usershome', name: 'home', component: UserHomeComponent, props: true }
+    ]
+  });
 
-    // mock up the user - this well eventually come from the database UMS (user management system)
-    user: {
-      isAdmin: false,
-      avatar: null,
-      isLoggedIn: true
+  const vm = new Vue({
+    data: {
+      authenticated: false,
+      administrator: false,
+      user: [],
+
+      //currentUser: {},
     },
 
-    // this data would also come from the database, but we'll just mock it up for now
-    videodata: [
-      { name: "Pulp Fiction", thumb: "pulpfiction.jpg", vidsource: "pulpfiction.mp4", description: "A Quentin Tarantino Classic." },
-      // { name: "Star Wars The Force Awakens", thumb: "forceawakens.jpg", vidsource: "forceawakens.mp4", description: "yet another star wars movie" },
-      // { name: "Stranger Things", thumb: "strangerthings.jpg", vidsource: "strangerthings.mp4", description: "don't get lost in the upside down" },
-      // { name: "Marvel's The Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "will they make black widow action figures this time?" },
-      { name: "Titanic", thumb: "titanic.jpg", vidsource: "titanic.mp4", description: "Rose falls in love with an artist by the name of Jack." },
-      { name: "Forest Gump", thumb: "forestgump.jpg", vidsource: "forestgump.mp4", description: "Run Forest, Run! A classic Tom Hanks film for the family." }
-    ],
+    methods: {
+      setAuthenticated(status, data) {
+        this.authenticated = status;
+        this.user = data;
+      },
 
-    videotitle: "video title goes here",
-    vidsource: "",
-    videodescription: "video description here",
-
-    showDetails: false
-  },
-
-  methods: {
-    setUserPrefs() {
-      // this is the preferences control, hit the api when ready (or use a component)
-      console.log('set user prefs here');
+      logout() {
+        // push user back to login page
+        this.$router.push({ path: "/login" });
+        this.authenticated = false;
+      }
     },
 
-    userLogin(){
-      //call the login route, or load the login component 
-      console.log('do login / logout on click');
+    router: router
+  }).$mount("#app");
 
-      //this is a ternary statement -> shorthand for if / else
-      // the expression evaluates to true or false - if it's true, set the value equal to 
-      // the left of the colon. If it's false, set the value equal to the right
-      this.user.isLoggedIn = (this.user.isLoggedIn) ? false : true;
-    },
+  router.beforeEach((to, from, next) => {
+    //console.log('router guard fired!', to, from, vm.authenticated);
 
-    showMovieDetails({name, vidsource, description}) {
-      // console.log('show these details: ', movie);
-
-      this.videotitle = name;
-      this.vidsource = vidsource;
-      this.videodescription = description;
-
-      //make the movie detials show up
-      this.showDetails = true;
+    if (vm.authenticated == false) {
+      next("/login");
+    } else {
+      next();
     }
-  }
-});
+  });
+})();
